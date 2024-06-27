a7afc023e93ec34407b7540d683f2083e54f2f9a
chatchai <kmitl50ite15engineer@gmail.com>
2024-06-27 23:20:13 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {Profiler} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function onRender(id, phase, actualDuration, baseDuration, startTime, commitTime) {
+	console.log("id=>", id);
+	console.log("phase=>", phase);
+	console.log("actualDuration=>", actualDuration);
+	console.log("baseDuration=>", baseDuration);
+	console.log("startTime=>", startTime);
+	console.log("commitTime=>", commitTime);
+	// Aggregate or log render timings...
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+	// <Profiler id="App" onRender={onRender}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	// </Profiler>
 );
 
 // If you want to start measuring performance in your app, pass a function
